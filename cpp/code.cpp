a43b7e6a6d985f081806e72a0f1dbf204c4585e5
hifzurrehman <YOUR_EMAILfk331998@gmail.com>
2024-12-30 15:42:37 +0530

    test

---
+++ cpp/code.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+
+
+
+
+
+
+
+// Example 1:
+// Input:N = 12345
+// Output:5
+// Explanation:  The number 12345 has 5 digits.
+// Example 2:
+// Input:N = 7789
+// Output: 4
+// Explanation: The number 7789 has 4 digits.
+
+
+int main() {
+
+		cout << "enter the vlaue for count digit";
+      
+
+        let n =   cin >> "";
+	
+    return 0;
+}
+
